071b13f289d6b476dc53dbe890e7e3eb3033a9b8
Edik89 <bereseduard@gmail.com>
2017-04-19 15:08:19 +0300

    delete_zip

---
+++ src/libs/newServer.js
@@ -0,0 +1,52 @@
+import Koa from 'koa';
+import config from '../config';
+import Router from 'koa-router';
+import koaBody from 'koa-body';
+import
+{
+  login,
+  logout,
+  register,
+  updateImage,
+  getUsers
+} from '../middleware/UsrPassport';
+import {
+  GreateOrganization,
+  reqToJoinOrganization,
+  getOrgs
+}
+from '../middleware/OrgPassport';
+import passport, { JwtStr } from "../middleware/passport";
+import error from "../middleware/error";
+
+export default async function createServer(port) {
+
+  const app = new Koa(),
+  router = new Router();
+
+  JwtStr(passport);
+
+  app
+    .use(error)
+    .use(koaBody({
+      multipart: true,
+      formLimit: "10000kb",
+      formidable: {  keepExtensions: true, uploadDir: config.get("root")+'/pic' }
+    }))
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .use(passport.initialize());
+
+  router
+    .post('/register', register)
+    .get('/users', getUsers)
+    .post('/login', login)
+    .get('/logout',  logout)
+    .put('/updateAvatar', updateImage)
+    .post('/greatOrg', GreateOrganization)
+    .get('/orgs', getOrgs)
+    .get('/org/:id', reqToJoinOrganization);
+
+     app.listen(port);
+
+}
